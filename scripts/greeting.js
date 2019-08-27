d336315ef72beb6b52ff2886a0bfbe75a5d80e5b
Came <carmen.sartorio@gmail.com>
2019-08-27 15:04:39 -0400

    complete clock

---
+++ scripts/greeting.js
@@ -0,0 +1,11 @@
+const form = document.querySelector(".js-form");
+const inout= form.querySelector("input");
+const greeting = document.querySelector (".js-greetings");
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function init() {
+
+}
+
+init();
