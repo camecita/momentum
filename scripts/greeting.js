cd9893c3494cb00a0c557e94a4c275d33d47bdc1
Came <carmen.sartorio@gmail.com>
2019-08-27 15:30:04 -0400

    make clock

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -4,8 +4,40 @@ const greeting = document.querySelector (".js-greetings");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
-function init() {
+function saveName(text) {
+    localstorage.setItem(USER_LS, text)
+}
 
+fuction handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() [
+    form.classList.add(SHOWING_CN);
+    form.addEventListener('submit',handleSubmit);
+]
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hola! ${text}`
+
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+        askForNsame();  
+    } else {
+      paintGreeting(currentUser);  
+    }
+}
+
+function init() {
+    loadName();
 }
 
 init();
